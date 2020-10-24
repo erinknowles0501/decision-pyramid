9ffd7e612a376a26ddb3f153f00c21fb29fe50a5
Erin <colorandcontrast@gmail.com>
2020-10-24 15:11:15 -0300

    Create option-creating interface

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <DecisionOption
+            v-for="(option, index) in options"
+            :key="index"
+            v-model="options[index]"
+        />
+        <DecisionPicker />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DecisionOption from "./components/DecisionOption.vue";
+import DecisionPicker from "./components/DecisionPicker.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        DecisionOption,
+        DecisionPicker,
+    },
+    data() {
+        return {
+            options: ["", "", "", "", ""],
+        };
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/DecisionOption.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div
+            @click="settingOption = true"
+            v-if="!settingOption"
+            style="cursor: default"
+        >
+            {{ localOption ? localOption : "Click here to set an option" }}
+        </div>
+        <div v-if="settingOption">
+            <input
+                type="text"
+                v-model="localOption"
+                @blur="settingOption = false"
+                @keydown.enter="settingOption = false"
+                ref="input"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "decision-picker",
+    props: ["value"],
+    data() {
+        return {
+            settingOption: false,
+        };
+    },
+    computed: {
+        localOption: {
+            get() {
+                return this.value;
+            },
+            set(val) {
+                this.$emit("input", val);
+            },
+        },
+    },
+    watch: {
+        settingOption(val) {
+            if (val === true) {
+                this.$nextTick(() => {
+                    this.$refs.input.focus();
+                    this.$refs.input.select();
+                });
+            }
+        },
+    },
+};
+</script>
